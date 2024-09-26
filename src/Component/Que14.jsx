71776038f6946add08b67efd43f9111d0e71b807
himani-vsnv <hvaishnav2112@gmail.com>
2024-09-26 15:15:46 +0530

    second change

---
+++ src/Component/Que14.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Que14 = () => {
+
+    let a=0;
+    let b=1;
+    for (let i=0;i <= 20;i++){
+        console.log9(a);
+        let c = a+b;
+        a = b;
+        b = c;
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Que14
